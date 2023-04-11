6d97ceeb2a3efc8f6de45b1e8a7461f2885b4f49
AminShK <aminshk4@gmail.com>
2023-04-11 15:05:55 +0430

    projectHooBank

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Business from "./Business";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Testimonial from "./Testimonial";
+
+export {
+    Navbar,
+    Hero,
+    Footer,
+    Business,
+    Billing,
+    CardDeal,
+    Clients,
+    CTA,
+    Stats,
+    Testimonial
+}
